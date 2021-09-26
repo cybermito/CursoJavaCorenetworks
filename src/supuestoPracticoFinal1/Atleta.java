3cf6c2fb62779f086d2dc02b32b46ea17d46b29d
cybermito <cybermito@gmail.com>
2021-09-26 06:29:29 +0200

    Ejercicio 1 - Supuesto Final 1. Declaramos las clases Atleta y Trofeo

--- src/supuestoPracticoFinal1/Atleta.java
+++ src/supuestoPracticoFinal1/Atleta.java
@@ -2,11 +2,18 @@ package supuestoPracticoFinal1;
 
 public class Atleta {
 
-	int numeroFederado
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//Definimos las variables necesarias. 
+	int numeroFederado;
+	String nombreAtleta;
+	double horasDeEntrenamiento;
+	String[] trofeosGanados = new String[5];
+		
+	//Creamos un tipo de dato personalizado con enum, este tipo es para asignar el tipo de Atleta. 
+	enum TipoAtleta {
+		VELOCISTA, MEDIO_FONDO, FONDO, OTROS
 	}
-
+	
+	
+	
+	
 }
---
+++ src/supuestoPracticoFinal1/Trofeo.java
@@ -0,0 +1,8 @@
+package supuestoPracticoFinal1;
+
+public class Trofeo {
+	String nombreTrofeo;
+	String descripciónTrofeo;
+	Boolean trofeoOficial;
+
+}
